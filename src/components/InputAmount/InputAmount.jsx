594b3e6393ff929843ab52adb3cd2c220a011243
James Kim <jamesjkim8806@gmail.com>
2021-03-29 13:59:09 -0700

    btc rate up and running

--- src/components/InputAmount/InputAmount.jsx
+++ src/components/InputAmount/InputAmount.jsx
@@ -5,10 +5,14 @@ import apiService from '../../utils/apiService';
 
 export default function InputAmount(){
 
+  const [btcData, setBTCData] = useState();
+  const [btcPrice, setBtcPrice] = useState(null);
+
   async function getBTCRate(){
     try {
       const data = await apiService.getBTCRate();
       console.log("data form btc rate: ", data.data[0].price);
+      setBtcPrice(data.data[0].price);
     } catch(err){
       console.log("ERROR FROM BTCRATE: ", err)
     }
@@ -17,10 +21,9 @@ export default function InputAmount(){
   useEffect(() => {
     getBTCRate();
   }, [])
-    //  getBTCRate();
   return(
     <>
-      <h1>Input Amount component</h1>
+      <h1>Input Amount component {btcPrice}</h1>
       <ConvertedAmount />
     </>
   )
