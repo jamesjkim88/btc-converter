a7cb31e3898572b5f24baaeb6e2ac2f26628fae7
James Kim <jamesjkim8806@gmail.com>
2021-04-05 16:51:06 -0700

    info copy updated

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -9,16 +9,16 @@ export default function ConvertedAmount(){
         <h2 className="info-copy info-header"><span className="red-border">About</span></h2>
         <p className="info-copy info-subhead">This tool can help you to:</p>
         <ul className="info-list">
-          <li className="info-list-item info-copy">Able to see curent market value of Bitcoin in U.S Dollar</li>
-          <li className="info-list-item info-copy">Convert Bitcoin (BTC) to U.S Dollar (USD)</li>
+          <li className="info-list-item info-copy">Able to see curent market value of the Top 10 Crypto Currencies in multiple world currencies</li>
+          <li className="info-list-item info-copy">Convert Bitcoin (BTC) to U.S Dollar (USD) or Litecoin (LTC) to Euro (EUR)</li>
         </ul>
       </section>
       <section className="usage">
         <h2 className="info-copy info-header"><span className="red-border">Usage</span></h2>
         <p className="info-copy info-subhead">Here is how you can use the tool:</p>
         <ul className="info-list">
-          <li className="info-list-item info-copy">Enter amount of Bitcoin. On the empty input field, you can simply enter the number amount of Bitcoins you would like to see in U.S Dollars</li>
-          <li className="info-list-item info-copy">This tool can help you decide how many Bitcoin(s) you want neccessary for you</li>
+          <li className="info-list-item info-copy">Enter amount of coins. On the dropdown menu you can select one of the Top 10 Crypto currencies and see market value on different world wide currencies</li>
+          <li className="info-list-item info-copy">This tool can help you decide how many coins you want neccessary for you</li>
           <li className="info-list-item info-copy">Try it on any mobile device! Fully responsive and designed for any devices</li>
           <li className="info-list-item info-copy">Bookmark this page for quick and efficient BTC-to-USD converter</li>
         </ul>
