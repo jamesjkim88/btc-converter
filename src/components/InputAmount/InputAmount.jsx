acbad3b3b4eaf42a0aa942d990fa776293b70c78
James Kim <jamesjkim8806@gmail.com>
2021-04-05 14:11:12 -0700

    state updated

--- src/components/InputAmount/InputAmount.jsx
+++ src/components/InputAmount/InputAmount.jsx
@@ -8,10 +8,21 @@ export default function InputAmount(){
   const [btcData, setBTCData] = useState();
   const [btcPrice, setBtcPrice] = useState(null);
   const [inputAmount, setInputAmount] = useState(null);
-  const [selectedFile, setSelectedFile] = useState('')
-  const [state, setState] = useState({
-    caption: ''
-  })
+  const [cashCurrency, setCashCurrency] = useState('USD');
+  const [cyrptoCurrency, setCryptoCurrency] = useState('BTC');
+
+  const cyrptoOptions = [
+    {key: "BTC", value: "BTC", text: "BTC"},
+    {key: "ETH", value: "ETH", text: "ETH"},
+    {key: "LTC", value: "LTC", text: "LTC"}
+  ]
+
+  const currencyOptions = [
+    {key: "USD", value: "USD", text: "USD"},
+    {key: "WON", value: "WON", text: "WON"},
+    {key: "EUR", value: "EUR", text: "EUR"}
+  ]
+
   async function getBTCRate(){
     try {
       const data = await apiService.getBTCRate();
@@ -28,30 +39,18 @@ export default function InputAmount(){
   function convertedAmount() {
     const formatter = new Intl.NumberFormat('en-US', {
       style: 'currency',
-      currency: 'USD'
+      currency: cashCurrency
     })
     const data = `${inputAmount * btcPrice}`
     return formatter.format(data)
   }
 
-//   const addressDefinitions = faker.definitions.address
-// const stateOptions = _.map(addressDefinitions.state, (state, index) => ({
-//   key: addressDefinitions.state_abbr[index],
-//   text: state,
-//   value: addressDefinitions.state_abbr[index],
-// }))
 
-  const cyrptoOptions = [
-    {key: "BTC", value: "BTC", text: "BTC"},
-    {key: "ETH", value: "ETH", text: "ETH"},
-    {key: "LTC", value: "LTC", text: "LTC"}
-  ]
 
-  const currencyOptions = [
-    {key: "USD", value: "USD", text: "USD"},
-    {key: "WON", value: "WON", text: "WON"},
-    {key: "EURO", value: "EURO", text: "EURO"}
-  ]
+  function handleOnChange(evt){
+    setCashCurrency(evt.target.textContent);
+    setCryptoCurrency(evt.target.textContent);
+  }
 
   useEffect(() => {
     getBTCRate();
@@ -59,13 +58,11 @@ export default function InputAmount(){
   return(
     <div className="conversion" id="conversion">
       <div className="row">
-        <Dropdown defaultValue="BTC" search selection options={cyrptoOptions} /> <span className="icon"><i className="fas fa-long-arrow-alt-right"></i></span> <Dropdown defaultValue="USD" search selection options={currencyOptions} />
+        <Dropdown defaultValue="BTC" search selection options={cyrptoOptions} onChange={handleOnChange} /> <span className="icon"><i className="fas fa-long-arrow-alt-right"></i></span> <Dropdown defaultValue="USD" search selection options={currencyOptions} onChange={handleOnChange} />
         {/* <h1 className="title">BTC <span className="icon"><i className="fas fa-long-arrow-alt-right"></i></span> USD</h1> */}
         <Input focus placeholder='# of BTC...' onChange={handleChange}/>
         <h1 className="title converted-amount">{ inputAmount ? convertedAmount() : "" }</h1>
       </div>
-   
-
     </div>
   )
 }
